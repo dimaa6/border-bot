4f693fd94ad819180a93c2046fc513fb8455d0d8
Dmytro Bohdanov <dimaa6@gmail.com>
2026-06-10 12:26:42 +0300

    Added other borders, minor improvements

---
+++ src/checkpoints.py
@@ -0,0 +1,54 @@
+COUNTRIES_AND_CHECKPOINTS = {
+    "PL": {
+        "name": "Польща",
+        "checkpoints": {
+            "PL_KRAKOVETS":     "Краківець",
+            "PL_SHEHYNI":       "Шегині",
+            "PL_RAVA":          "Рава-Руська",
+            "PL_USTYLUH":       "Устилуг",
+            "PL_UHRYNIV":       "Угринів",
+            "PL_HRUSHIV":       "Грушів",
+            "PL_NIZHANKOVICHI": "Нижанковичі",
+            "PL_SMILNYTSIA":    "Смільниця",
+        },
+    },
+    "SK": {
+        "name": "Словаччина",
+        "checkpoints": {
+            "SK_MALYI_BEREZNYI": "Малий Березний",
+            "SK_UZHHOROD":       "Ужгород",
+            "SK_MALI_SELMENCI":  "Малі Селменці",
+        },
+    },
+    "RO": {
+        "name": "Румунія",
+        "checkpoints": {
+            "RO_PORUBNE":      "Порубне",
+            "RO_SOLOTVYNO":    "Солотвино",
+            "RO_DIAKOVE":      "Дякове",
+            "RO_DIAKIVTSI":    "Дяківці",
+            "RO_KRASNOILSK":   "Красноїльськ",
+        },
+    },
+    "HU": {
+        "name": "Угорщина",
+        "checkpoints": {
+            "HU_LUZHANKA":  "Лужанка",
+            "HU_CHOP":      "Чоп (Тиса)",
+            "HU_VYLOK":     "Вилок",
+            "HU_DZVINKOVE": "Дзвінкове",
+            "HU_KOSYNO":    "Косино",
+        },
+    },
+    "MD": {
+        "name": "Молдова",
+        "checkpoints": {
+            "MD_MOHYLIV":    "Могилів-Подільський",
+            "MD_MAIAKY":     "Маяки-Удобне",
+            "MD_ROSSOSHANY": "Россошани",
+            "MD_MAMALYHA":   "Мамалига",
+            "MD_SOKYRIANY":  "Сокиряни",
+            "MD_BRONNYTSIA": "Бронниця",
+        },
+    },
+}
